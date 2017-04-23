a3718bd1d8da32316689c2856cc4390f5061bf3b
Justin Bartlett <jbar@Justins-MacBook-Pro.local>
2017-04-23 17:45:37 -0400

    add firebase

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,7 +3,18 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
+import { AngularFireModule } from 'angularfire2';
+
 import { AppComponent } from './app.component';
+
+export const firebaseConfig = {
+  apiKey: "AIzaSyDv59aLmPzrl_D_tRJ_J-dNCRcd1CT-Dgc",
+  authDomain: "justinguru.firebaseapp.com",
+  databaseURL: "https://justinguru.firebaseio.com",
+  projectId: "firebase-justinguru",
+  storageBucket: "firebase-justinguru.appspot.com",
+  messagingSenderId: "432190520908"
+};
 
 @NgModule({
   declarations: [
@@ -12,7 +23,8 @@ import { AppComponent } from './app.component';
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    AngularFireModule.initializeApp(firebaseConfig)
   ],
   providers: [],
   bootstrap: [AppComponent]
